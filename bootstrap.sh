e24bcc1ff03ad1ce030c8bb2f8b219ae6bcfb6c6
Joce <go.joce@gmail.com>
2019-03-07 00:30:29 -0600

    Followed instructions from hw 7

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,15 +8,8 @@ sudo yum -y install wget
 sudo yum -y install dos2unix
 
 # download java
-sudo wget --no-cookies \
---no-check-certificate \
---header "Cookie: oraclelicense=accept-securebackup-cookie" \
-"http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-x64.rpm" \
--O /tmp/jdk-8-linux-x64.rpm
-
+sudo wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm -O /tmp/jdk-8-linux-x64.rpm
+	
 # install java
 sudo yum -y install /tmp/jdk-8-linux-x64.rpm
 sudo rm /tmp/jdk-8-linux-x64.rpm
-
-# run the application
-#java -jar /tmp/spring-petclinic-1.4.2.jar &
